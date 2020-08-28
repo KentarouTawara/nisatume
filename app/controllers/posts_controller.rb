9ab30d019147261b93cf98d7a5d8be89b24db496
KentarouTawara <sironagasuken2@gmail.com>
2020-08-28 10:21:57 +0900

    [add]Postモデル・コントローラーの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def new
-
+    @post = Post.new
   end
 
   def search
@@ -11,4 +11,9 @@ class PostsController < ApplicationController
     end
     render :new
   end
+
+  def create
+    @linking_book = LinkingBook
+  end
+
 end
