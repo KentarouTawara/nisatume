188780a02b074343a4d07880d9ba406902ab9b8e
KentarouTawara <sironagasuken2@gmail.com>
2020-09-01 22:50:41 +0900

    [add]入力データをコントローラーに送って保存

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,14 @@
 class PostsController < ApplicationController
-  def new
-    @post = Post.new
+  def new; end
+
+  def create
+    @post = current_user.posts.create
+    @linking_book = Book.create(linking_book_params)
+    @linked_book = Book.create(linked_book_params)
+    @linking_book_content = LinkingBook.create(post_id: @post.id, book_id: @linking_book.id, content:
+      params[:linking][:content])
+    @linked_book_content = LinkedBook.create(post_id: @post.id, book_id: @linked_book.id, content:
+      params[:linked][:content])
   end
 
   def search
@@ -11,5 +19,15 @@ class PostsController < ApplicationController
       @linked_books = RakutenWebService::Books::Book.search(title: params[:keyword])
       @role = params[:role]
     end
+  end
+
+  private
+
+  def linking_book_params
+    params.require(:linking).permit(:title, :author, :isbn, :publisher, :book_image, :book_url)
+  end
+
+  def linked_book_params
+    params.require(:linked).permit(:title, :author, :isbn, :publisher, :book_image, :book_url)
   end
 end
--- app/javascript/javascripts/choice.js
+++ app/javascript/javascripts/choice.js
@@ -1,33 +1,91 @@
 
 $(function () {
-    $(document).on('click','.js-button-choice-linking',function (e) {
+    $(document).on('click','.js-button-choice-linking', function (e) {
         e.preventDefault();
         const isbn = $(this).data('book-id')
-        console.log(this)
-        const choiceBook = $('#id-linking-' + isbn);
+        const choiceBook = $('#id-linking-' + isbn).addClass('linking-book')
 
         $(".list-item-linking").hide();
         $(choiceBook).show()
 
-        $("#post_form_linking_book_title").val($("#js-book-title-" + isbn).html());
-        $("#post_form_linking_book_author").val($("#js-book-author-" + isbn).html());
-        $("#post_form_linking_book_isbn").val($("#js-book-isbn-" + isbn).html());
-        $("#post_form_linking_book_publisher").val($("#js-book-publisher-" + isbn).html());
-        const image = $("#js-book-image-" + isbn).attr('src')
-        console.log(image)
-        $("#post_form_linking_book_image").val(image);
-        $("#post_form_linking_book_url").val($("#js-book-url-" + isbn).html());
+        $('#linking_title').val($("#js-linking-title-" + isbn).html())
 
+        $("#js-linking-title-" + isbn).attr('id', 'linking-title');
+        $("#js-linking-author-" + isbn).attr('id', 'linking-author');
+        $("#js-linking-isbn-" + isbn).attr('id', 'linking-isbn');
+        $("#js-linking-publisher-" + isbn).attr('id', 'linking-publisher');
+        $("#js-linking-image-" + isbn).attr('id', 'linking-image');
+        $("#js-linking-url-" + isbn).attr('id', 'linking-url');
     })
 });
 
 $(function () {
-    $(document).on('click','.js-button-choice-linked',function (e) {
+    $(document).on('click','.js-button-choice-linked', function (e) {
         e.preventDefault();
         const isbn = $(this).data('book-id')
         const choiceBook = $('#id-linked-' + isbn);
 
         $(".list-item-linked").hide();
         $(choiceBook).show()
+
+        $("#js-linked-title-" + isbn).attr('id', 'linked-title');
+        $("#js-linked-author-" + isbn).attr('id', 'linked-author');
+        $("#js-linked-isbn-" + isbn).attr('id', 'linked-isbn');
+        $("#js-linked-publisher-" + isbn).attr('id', 'linked-publisher');
+        $("#js-linked-image-" + isbn).attr('id', 'linked-image');
+        $("#js-linked-url-" + isbn).attr('id', 'linked-url');
     })
+});
+
+$(function () {
+    $(document).on('click', '.submit-button', function (e) {
+        e.preventDefault();
+        const linkingTitle = $("#linking-title").text()
+        const linkingAuthor = $("#linking-author").text()
+        const linkingIsbn = $("#linking-isbn").text()
+        const linkingPublisher = $("#linking-publisher").text()
+        const linkingImage = $("#linking-image").attr('src')
+        const linkingUrl = $("#linking-url").attr('href')
+        const linkingContent = $("#linking_content").val()
+
+        const linkedTitle = $("#linked-title").text()
+        const linkedAuthor = $("#linked-author").text()
+        const linkedIsbn = $("#linked-isbn").text()
+        const linkedPublisher = $("#linked-publisher").text()
+        const linkedImage = $("#linked-image").attr('src')
+        const linkedUrl = $("#linked-url").attr('href')
+        const linkedContent = $("#linked_content").val()
+
+        $.ajax({
+            type: "POST",
+            url: "/posts",
+            data: {
+                linking: {
+                    title: linkingTitle,
+                    author: linkingAuthor,
+                    isbn: linkingIsbn,
+                    publisher: linkingPublisher,
+                    book_image: linkingImage,
+                    book_url: linkingUrl,
+                    content: linkingContent
+                },
+                linked: {
+                    title: linkedTitle,
+                    author: linkedAuthor,
+                    isbn: linkedIsbn,
+                    publisher: linkedPublisher,
+                    book_image: linkedImage,
+                    book_url: linkedUrl,
+                    content: linkedContent
+                }
+            }
+        }).done(function (data) {
+            console.log('ok')
+            console.log(data)
+
+        }).fail(function (data) {
+            console.log('bad')
+            console.log(data)
+        })
+    });
 });
