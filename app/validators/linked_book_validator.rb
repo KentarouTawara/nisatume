ea898c997b09a77c04f4d17b0e77c14cf2247b1f
KentarouTawara <sironagasuken2@gmail.com>
2020-09-08 00:36:30 +0900

    [fix]validationの変更

--- app/validators/linked_book_validator.rb
+++ app/validators/linked_book_validator.rb
@@ -1,7 +1,10 @@
 class LinkedBookValidator < ActiveModel::Validator
   def validate(record)
-    if record.linked_title.blank? || record.linked_author.blank? || record.linked_isbn || record.linked_publisher || record.linked_image || record.linked_url
-      record.errors[:base] << '二冊目を選んでください'
+    if record.linked_title.blank? || record.linked_author.blank? || record.linked_isbn.blank? || record
+                                                                                                 .linked_publisher
+                                                                                                   .blank? || record
+                                                                                                              .linked_image.blank? || record.linked_url.blank?
+      record.errors[:linked_book] << '二冊目を選んでください'
     end
   end
 end
--- app/validators/linking_book_validator.rb
+++ app/validators/linking_book_validator.rb
@@ -1,7 +1,10 @@
 class LinkingBookValidator < ActiveModel::Validator
   def validate(record)
-    if record.linking_title.blank? || record.linking_author.blank? || record.linking_isbn || record.linking_publisher || record.linking_image || record.linking_url
-      record.errors[:base] << '一冊目を選んでください'
+    if record.linking_title.blank? || record.linking_author.blank? || record.linking_isbn.blank? || record
+                                                                                                   .linking_publisher
+                                                                                                   .blank? || record
+                                                                                                                .linking_image.blank? || record.linking_url.blank?
+      record.errors[:linking_book] << '一冊目を選んでください'
     end
   end
 end
