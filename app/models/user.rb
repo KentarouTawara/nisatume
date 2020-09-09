5b338d71dcad09560643099f1e31e16ed7ad45a4
KentarouTawara <sironagasuken2@gmail.com>
2020-09-10 00:02:27 +0900

    [add]編集権限の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
   validates :email, uniqueness: true
+
+  def own?(object)
+    id == object.user_id
+  end
 end
