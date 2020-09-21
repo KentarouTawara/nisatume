9b2a348744d6f5a052f01266d59d81c387bf747e
KentarouTawara <sironagasuken2@gmail.com>
2020-09-21 11:58:13 +0900

    [fix]routes.rbのアクションの絞り込み

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
-  resources :users
-  resource :mypage
+  resources :users, only: %i[new create]
+  resource :mypage, only: %i[show]
   resources :posts do
     collection do
       get :search
